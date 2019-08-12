97259e6ab20037f4ab826571b79052f71396a85e
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-08-12 12:22:05 +0200

    Added OnTick method to TankPlayerController class.

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,10 +3,13 @@
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
-
 	auto ControlledTank = GetControlledTank();
 	if (ControlledTank) UE_LOG(LogTemp, Warning, TEXT("Player Tank: %s"), *ControlledTank->GetName())
 	else UE_LOG(LogTemp, Error, TEXT("No Tank possessed!"))
+}
+
+void ATankPlayerController::Tick(float DeltaTime){
+	Super::Tick(DeltaTime);
 }
 
 ATank* ATankPlayerController::GetControlledTank() const {
@@ -14,3 +17,9 @@ ATank* ATankPlayerController::GetControlledTank() const {
 
 }
 
+void ATankPlayerController::AimTowardsCrosshair() {
+	if (!GetControlledTank()) return;
+	// Get world location if linetrace through crosshair
+	// If it hits the landscape
+		// Tell controlled tank to aim at this point
+}
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -5,12 +5,15 @@
 #include "Public/Tank.h"
 #include "TankPlayerController.generated.h" // Must be last include
 
-
 UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 public:
+	void BeginPlay() override;
+	void Tick(float) override;
+private:
 	ATank* GetControlledTank() const;
-	virtual void BeginPlay() override;
+	// Start the tank moving the barrel so that a shot would hit where the crosshair intersects the world
+	void AimTowardsCrosshair();
 };
