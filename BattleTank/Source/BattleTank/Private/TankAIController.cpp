b359d6d8bb0a558b2cac500189c8ef0277cd0f4e
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-08-12 12:06:06 +0200

    TankAIController gets player tank.

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,15 +1,17 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "Public/TankAIController.h"
 
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
-	auto ControlledAITank = GetPossessedByAITank();
-	if(ControlledAITank) UE_LOG(LogTemp,Warning,TEXT("AI Tank: %s"),*ControlledAITank->GetName())
-	else UE_LOG(LogTemp,Error,TEXT("No tank is possessed by AI!"))
+	auto PlayerTank = GetPlayerTank();
+	if (PlayerTank) UE_LOG(LogTemp, Warning, TEXT("AIController: Player Tank: %s"), *PlayerTank->GetName())
+	else UE_LOG(LogTemp,Error,TEXT("AIController:: No Player Tank is found!"))
 }
 
 ATank* ATankAIController::GetPossessedByAITank() const {
 	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const {
+	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
 }
--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -1,19 +1,20 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "AIController.h"
 #include "Public/Tank.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 #include "TankAIController.generated.h"
-
 
 UCLASS()
 class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 public:
+	void BeginPlay() override;
+
+private:
 	ATank* GetPossessedByAITank() const;
-	virtual void BeginPlay() override;
-	
+	ATank* GetPlayerTank() const;
 };
