20e1cfd3956bbf5f193dc394286fe98ee3957717
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-08-19 23:23:49 +0200

    Added HealthBar and for first time used BlueprintPure( like const method in C++)

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -14,10 +14,14 @@ private:
 	ATank();
 	UPROPERTY(EditDefaultsOnly,Category="Setup")
 	int32 StartingHealth = 100;
-
 	UPROPERTY(VisibleAnywhere,Category = "Health")
 	int32 CurrentHealth = StartingHealth;
 public:
+
+	// Return current health as a percentage of starting health, between 0 and 1
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetHealthPercent() const;
+
 	// Called by the engine when actor damage is dealt
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
 };
